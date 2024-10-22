81354eee9ac3ed003d0ec064200e6e9d670f025e
AlessioBarsi <alessiobarsi98@gmail.com>
2024-10-22 10:47:00 +0200

    Created User.jsx

---
+++ frontend/src/User.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+import { useNavigate } from 'react-router-dom';
+import axios from "axios";
+function User() {
+    const [user, setUser] = useState({});
+    const navigate = useNavigate();
+    useEffect(() => {
+        axios.get('user')
+        .then(response => setUser(response.data));
+     }, []);
+
+    function handleLogout() {
+        axios.post('logout')
+        .finally(() => {
+           localStorage.removeItem('token');
+           axios.defaults.headers.common['Authorization'] = 'Bearer';
+           navigate('/login');
+        });
+    }
+
+    return (
+        <div>
+           <div>User ID: {user.id}</div>
+           <div>Email: {user.email}</div>
+           <button onClick={handleLogout}>Logout</button>
+        </div>
+     );
+}
+
+export default User;

6ac6e5b37a9ef545cc8188c01debd4eb607fbf7c
AlessioBarsi <alessiobarsi98@gmail.com>
2024-10-22 10:30:04 +0200

    Cleaned App.jsx and removed LoginForm.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,14 @@
 import { createContext, useState, useContext, useEffect } from 'react';
 import './App.css';
 import MainContent from './MainContent';
-import LoginForm from './LoginForm';
 
 export const UserContext = createContext(null);
 
 function App() {
-  const [user, setUser] = useState('Username');
-  const [login, isLoggedIn] = useState(false)
-
-  useEffect(() => {
-    setUser('Name');
-  }, []); 
 
   return (
     <div>
       Homepage
-      <UserContext.Provider value={user}>
-
-        {
-          login ? 
-          <div>
-            <MainContent />
-            User is logged in</div> : 
-          <div>Please login
-            <LoginForm></LoginForm>
-          </div>
-        }
-      </UserContext.Provider>
-
     </div>
   );
 }

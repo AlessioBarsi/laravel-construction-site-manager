a55b0bb12315d8dbd88185c623f362f47e6ec5c9
AlessioBarsi <alessiobarsi98@gmail.com>
2024-12-09 14:43:39 +0100

    Updated Home.jsx

--- frontend/src/AuthContext.jsx
+++ frontend/src/AuthContext.jsx
@@ -1,5 +1,4 @@
 import React, { createContext, useState, useContext, useEffect } from 'react';
-import { userService } from './api/users';
 
 const AuthContext = createContext();
 
--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -3,7 +3,6 @@ import NewReportModal from './Modals/NewReportModal.jsx';
 import { userService } from './api/users.jsx';
 
 import Button from '@mui/material/Button';
-import Grid from '@mui/material/Grid2';
 import Stack from '@mui/material/Stack';
 
 import { AccountCircle } from '@mui/icons-material';
@@ -20,7 +19,7 @@ export default function Home() {
     const handleOpen = () => setOpen(true);
 
     const navigate = useNavigate();
-    
+
     //User Data
     const { userId } = useAuth();
     const [userData, setUserData] = useState(null);
@@ -40,27 +39,19 @@ export default function Home() {
     }, [userId]);
 
     return (
-        <div className='mt-5 ml-10 mr-10'>
-            <Grid container spacing={20}>
-                <Grid size={4}>
+        <div className='mt-5 ml-10 mr-10 flex justify-between'>
                     <Stack spacing={2} direction='row'>
                         <><Button variant='outlined' startIcon={<EditIcon />} onClick={handleOpen}>New Report</Button>
                             <NewReportModal open={open} handleClose={handleClose} user={userData} /></>
                     </Stack>
-                </Grid>
 
-                <Grid size={4}>
                     <Stack spacing={2} direction='row'>
                         <Button variant='contained' startIcon={<AccountCircle />} onClick={() => navigate('/profile')}>User Profile</Button>
                     </Stack>
-                </Grid>
 
-                <Grid size={4}>
                     <Stack spacing={2} direction='row'>
                         <Button variant='outlined' startIcon={<InsertDriveFileIcon />} onClick={() => navigate('/reports')}>Your Reports</Button>
                     </Stack>
-                </Grid>
-            </Grid>
         </div>
     );
 }

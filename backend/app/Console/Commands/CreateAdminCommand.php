3488d902442dd0c74dde4948dc8f3ac0a87691d6
AlessioBarsi <alessiobarsi98@gmail.com>
2024-12-16 15:33:00 +0100

    Added command for admin creation, set role column in users table to nullable

---
+++ backend/app/Console/Commands/CreateAdminCommand.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+class CreateAdminCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'admin:create';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Create a new user with admin permissions';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $email = $this->ask('Enter email address:');
+        $password = $this->secret('Enter password:');
+        $passwordConfirmation = $this->secret('Confirm password:');
+        //Validate inputs
+        $validator = Validator::make([
+            'email' => $email,
+            'password' => $password,
+            'password_confirmation' => $passwordConfirmation
+        ], [
+            'email' => ['required', 'email', 'unique:users,email'],
+            'password' => ['required', 'min:8', 'same:password_confirmation']
+        ]);
+
+        if ($validator->fails()) {
+            foreach ($validator->errors()->all() as $error) {
+                $this->error($error);
+            }
+            return 1;
+        }
+        // Create user
+        $user = User::create([
+            'email' => $email,
+            'password' => Hash::make($password),
+            'first_name' => $this->ask('First name:', 'User'),
+            'last_name' => $this->ask('Last name:', '?'),
+            'admin' => true,
+        ]);
+
+        $this->info("User created successfully with email: {$email}");
+        return 0;
+    }
+}
